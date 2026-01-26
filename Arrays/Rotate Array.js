cfb27f4784d2181ede51b9774e93abda32932771
Pranav <mohnapranav@gmail.com>
2026-01-26 18:10:57 +0530

    submit

---
+++ Arrays/Rotate Array.js
@@ -0,0 +1,17 @@
+var rotate = function(nums, k) {
+    k = k % nums.length;
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+
+function reverse(nums, left, right) {
+    while (left < right) {
+        let temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+    }
+}
